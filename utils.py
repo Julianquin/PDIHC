6b1c45c1fe7a669d62eeb53ada8975d40b03b145
julian_quintero <amio0221@gmail.com>
2024-12-22 02:51:02 +0100

    baseline en utils, no usado hasta ahora

--- utils.py
+++ utils.py
@@ -4,7 +4,106 @@ from tqdm import tqdm
 from statsmodels.tsa.forecasting.theta import ThetaModel
 import matplotlib.pyplot as plt
 
+# =====================
+#  Baseline Methods
+# =====================
 
+def trailing_window(
+    scores,
+    alpha,
+    lr, # Dummy argument
+    weight_length,
+    ahead,
+    *args,
+    **kwargs
+):
+    T_test = scores.shape[0]
+    qs = np.zeros((T_test,))
+    for t in tqdm(range(T_test)):
+        t_pred = t - ahead + 1
+        if min(weight_length, t_pred) < np.ceil(1/alpha):
+            qs[t] = np.infty
+        else:
+            qs[t] = np.quantile(scores[max(t_pred-weight_length,0):t_pred], 1-alpha, method='higher')
+    results = {"method": "Trail", "q" : qs}
+    return results
+
+def aci_clipped(
+    scores,
+    alpha,
+    lr,
+    window_length,
+    T_burnin,
+    ahead,
+    *args,
+    **kwargs
+):
+    T_test = scores.shape[0]
+    alphat = alpha
+    qs = np.zeros((T_test,))
+    alphas = np.ones((T_test,)) * alpha
+    covereds = np.zeros((T_test,))
+    for t in tqdm(range(T_test)):
+        t_pred = t - ahead + 1
+        clip_value = scores[max(t_pred-window_length,0):t_pred].max() if t_pred > 0 else np.infty
+        if t_pred > T_burnin:
+            # Setup: current gradient
+            if alphat <= 1/(t_pred+1):
+                qs[t] = np.infty
+            else:
+                qs[t] = np.quantile(scores[max(t_pred-window_length,0):t_pred], 1-np.clip(alphat, 0, 1), method='higher')
+            covereds[t] = qs[t] >= scores[t]
+            grad = -alpha if covereds[t_pred] else 1-alpha
+            alphat = alphat - lr*grad
+
+            if t < T_test - 1:
+                alphas[t+1] = alphat
+        else:
+            if t_pred > np.ceil(1/alpha):
+                qs[t] = np.quantile(scores[:t_pred], 1-alpha)
+            else:
+                qs[t] = np.infty
+        if qs[t] == np.infty:
+            qs[t] = clip_value
+    results = { "method": "ACI (clipped)", "q" : qs, "alpha" : alphas}
+    return results
+
+def aci(
+    scores,
+    alpha,
+    lr,
+    window_length,
+    T_burnin,
+    ahead,
+    *args,
+    **kwargs
+):
+    T_test = scores.shape[0]
+    alphat = alpha
+    qs = np.zeros((T_test,))
+    alphas = np.ones((T_test,)) * alpha
+    covereds = np.zeros((T_test,))
+    for t in tqdm(range(T_test)):
+        t_pred = t - ahead + 1
+        if t_pred > T_burnin:
+            # Setup: current gradient
+            if alphat <= 1/(t_pred+1):
+                qs[t] = np.infty
+            else:
+                qs[t] = np.quantile(scores[max(t_pred-window_length,0):t_pred], 1-np.clip(alphat, 0, 1), method='higher')
+            covereds[t] = qs[t] >= scores[t]
+            grad = -alpha if covereds[t_pred] else 1-alpha
+            alphat = alphat - lr*grad
+
+            if t < T_test - 1:
+                alphas[t+1] = alphat
+        else:
+            if t_pred > np.ceil(1/alpha):
+                qs[t] = np.quantile(scores[:t_pred], 1-alpha)
+            else:
+                qs[t] = np.infty
+    results = { "method": "ACI", "q" : qs, "alpha" : alphas}
+    return results
 
 # =====================
 #  Preparación de Datos
@@ -500,9 +599,6 @@ def apply_pdi_with_calibration_with_diagnostics(
 #  Evaluación de Resultados
 # =========================
 
-# =============================
-#  Funciones de Evaluación
-# =============================
 def calculate_marginal_coverage(df, value_col, lower_col, upper_col):
     """
     Calcula la cobertura marginal de los intervalos.
